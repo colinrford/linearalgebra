05da03e49cacc755b16d947fed567ed032a43891
Colin Ford <ford31@purdue.edu>
2015-02-10 23:53:58 -0500

    forming foundations still. Implementation of vector class nearly done.

---
+++ vectorspace/vector.h
@@ -0,0 +1,53 @@
+
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+#include <iostream>
+
+struct vect {
+  const double* arrow;
+  int dimension;                        // dimension of vector; number of elements
+};
+
+class Vector {
+
+  vect _vector;
+
+  public:
+
+    Vector(int dim);                    // create a unit vector of dimension dim
+
+    Vector(int dim, const double elem); // create a vector with elements elem and dimension dim
+
+    double norm();                      // norm of this = v; ||v||
+
+    double dot(Vector v2);              // this = v1 dot with v2; v1 * v2
+
+    Vector cross(Vector v2);            // cross this = v1 and v2; v1 x v2
+
+    Vector add(Vector v2);              // add this = v1 and v2; v1 + v2
+
+    Vector subtract(Vector v2);         // subtract v2 from this = v1; v1 - v2
+
+    Vector scalarM(int s);              // multiply by scalar; s * this = v1
+
+    Vector scalarD(int s);              // divide by scalar; this = v1 / s
+
+    Vector unit();                      // returns the unit vector of given vector this = v; v / ||v||
+
+    bool equals(Vector v2);             // determines whether two vectors are equal
+
+    Vector operator+(const Vector &v);
+
+    Vector operator-(const Vector &v);
+
+    Vector operator*(const double s);
+
+    Vector operator/(const double s);
+
+    Vector operator==(const Vector &v);
+
+    void print();               // print vector contents (x1, x2, ..., xn, ...)
+
+    ~Vector();
+};
